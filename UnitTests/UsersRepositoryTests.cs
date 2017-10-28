37929a061ab3bc184eb1cc2fea443272f02e5438
Hlynur Stefánsson <hlynurs15@ru.is>
2017-10-28 14:14:07 +0000

    Add UnitTests Project

---
+++ UnitTests/UsersRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using LibraryAPI.Models.ViewModels;
+using LibraryAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryAPI.UnitTests
+{
+    [TestClass]
+    public class UsersRepositoryTests
+    {
+        private const string NAME_DABS  = "Daníel B. Sigurgeirsson";
+        private const string ADDRESS_DABS  = "Rauðrófustígur 14";
+        private const string EMAIL_DABS  = "dabs@ru.is";
+        private const string PHONE_DABS = "1234567";
+
+        [TestMethod]
+        public void AddUserToDatabase()
+        {
+            var options = new DbContextOptionsBuilder<AppDataContext>()
+                .UseInMemoryDatabase(databaseName: "Add_writes_to_database")
+                .Options;
+
+            // Run the test against one instance of the context
+            using (var context = new AppDataContext(options))
+            {
+                var service = new UsersRepository(context);
+                service.AddUser(new UserView {
+                    Name = NAME_DABS,
+                    Address = ADDRESS_DABS,
+                    Email = EMAIL_DABS,
+                    PhoneNumber = PHONE_DABS
+                });
+            }
+
+            // Use a separate instance of the context to verify correct data was saved to database
+            using (var context = new AppDataContext(options))
+            {
+                Assert.AreEqual(1, context.Users.Count());
+                Assert.AreEqual(NAME_DABS, context.Users.Single().Name);
+            }
+        }
+    }
+}
