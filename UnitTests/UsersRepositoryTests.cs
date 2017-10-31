dfa3276a393d7c5d2c5c396cfb3d36f996302ce2
Gudjon <gudjonss12@ru.is>
2017-10-31 20:13:21 +0000

    i dont even, hopefuly it works

--- UnitTests/UsersRepositoryTests.cs
+++ UnitTests/UsersRepositoryTests.cs
@@ -1,159 +1,259 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
-using LibraryAPI.Models.DTOModels;
-using LibraryAPI.Models.ViewModels;
-using LibraryAPI.Models.EntityModels;
-using Microsoft.EntityFrameworkCore;
 using LibraryAPI.Exceptions;
+using LibraryAPI.Models.ViewModels;
+using LibraryAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryAPI.Models.EntityModels;
 
-namespace LibraryAPI.Repositories
+namespace LibraryAPI.UnitTests.UsersRepositoryTests
 {
-    public class UsersRepository : IUsersRepository
+    [TestClass]
+    public class UsersRepositoryTests
     {
-        private readonly AppDataContext _db;
+        #region UserInfo
+        private const string NAME_DABS = "Daníel B. Sigurgeirsson";
+        private const string ADDRESS_DABS = "Rauðrófustígur 14";
+        private const string EMAIL_DABS = "dabs@ru.is";
+        private const string PHONE_DABS = "1234567";
+        private const bool DELETED_DABS = false;
+        #endregion
 
-        public UsersRepository(AppDataContext db) {
-            _db = db;
+        #region BookInfo
+        private const string TITLE_LOTR = "The Lord of The Rings";
+        private const string AUTHOR_LOTR = "J.R.R. Tolkien";
+        private DateTime RELEASE_LOTR = new DateTime(1954,6,29);
+        private const string ISBN_LOTR = "123456789";
+        private const bool AVAILABLE_LOTR = true;
+        private const bool DELETED_LOTR = false;
+        #endregion
+
+        #region LoanInfo
+        private const int USERID_LOAN = 1;
+        private const int BOOKID_LOAN = 1;
+        private DateTime STARTDATE_LOAN = new DateTime(2017,10,29);
+        private const bool RETURNED_LOAN = false;
+        private DateTime? ENDDATE_LOAN = null;
+        #endregion
+
+        private AppDataContext context;
+
+        [TestInitialize]
+        public void InitializeTest() {
+            var options = new DbContextOptionsBuilder<AppDataContext>()
+                .UseInMemoryDatabase(databaseName: "UsersDB")
+                .Options;
+
+            context = new AppDataContext(options);
+
+            context.Users.Add(new User {
+                Name = NAME_DABS,
+                Address = ADDRESS_DABS,
+                Email = EMAIL_DABS,
+                PhoneNumber = PHONE_DABS,
+                Deleted = DELETED_DABS
+            });
+
+            context.Books.Add(new Book {
+                Title = TITLE_LOTR,
+                Author = AUTHOR_LOTR,
+                ReleaseDate = RELEASE_LOTR,
+                ISBN = ISBN_LOTR,
+                Available = AVAILABLE_LOTR,
+                Deleted = DELETED_LOTR
+            });
+
+            context.Loans.Add(new Loan {
+                UserId = USERID_LOAN,
+                BookId = BOOKID_LOAN,
+                LoanDate = STARTDATE_LOAN,
+                EndDate = ENDDATE_LOAN,
+                HasBeenReturned = RETURNED_LOAN
+            });
+
+            context.SaveChanges();
         }
 
-        public UserDTO AddUser(UserView user) {
-            var userCheck = (from u in _db.Users
-                            where user.Email == u.Email
-                            && u.Deleted == false
-                            select u).SingleOrDefault();
-            
-            if (userCheck != null) {
-                throw new AlreadyExistsException("The request could not be completed due to a conflict with the current state of the resource.");
-            }
+        [TestCleanup]
+        public void CleanupTest() {
+            context.Database.EnsureDeleted();
+        }
 
-            var userEntity = new User {
-                Name = user.Name,
-                Address = user.Address,
-                Email = user.Email,
-                PhoneNumber = user.PhoneNumber,
+        [TestMethod]
+        public void Users_AddUser_OneUser()
+        {
+            // Arrange
+            var repo = new UsersRepository(context);
+            var user = new UserView {
+                Name = "Hlynur",
+                Address = "Laufrimi 85",
+                Email = "hlynurs15@ru.is",
+                PhoneNumber = "823-8028"
+            };
+
+            // Act
+            repo.AddUser(user);
+
+            // Assert
+            Assert.AreEqual(2, context.Users.Count());
+            Assert.AreEqual("Hlynur", context.Users.Where(x => x.Name == "Hlynur").SingleOrDefault().Name);
+            Assert.AreEqual("Laufrimi 85", context.Users.Where(x => x.Address == "Laufrimi 85").SingleOrDefault().Address);
+            Assert.AreEqual("hlynurs15@ru.is", context.Users.Where(x => x.Email == "hlynurs15@ru.is").SingleOrDefault().Email);
+            Assert.AreEqual("823-8028", context.Users.Where(x => x.PhoneNumber == "823-8028").SingleOrDefault().PhoneNumber);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(AlreadyExistsException))]
+        public void Users_AddUser_SameUserTwice()
+        {
+            // Arrange
+            var repo = new UsersRepository(context);
+            var user = new UserView {
+                Name = "Hlynur",
+                Address = "Laufrimi 85",
+                Email = "hlynurs15@ru.is",
+                PhoneNumber = "823-8028"
+            };
+
+            // Act
+            repo.AddUser(user);
+            repo.AddUser(user);
+
+            // Assert
+            Assert.Fail("Should have thrown an AlreadyExistsException");            
+        }
+
+        [TestMethod]
+        public void Users_GetUsers_GetTwoUsers() {
+            // Arrange
+            var repo = new UsersRepository(context);
+            context.Users.Add(new User {
+                Name = "Jón",
+                Address = "Dúfnahólar 10",
+                Email = "jon15@ru.is",
+                PhoneNumber = "123-4567",
                 Deleted = false
+            });
+            context.SaveChanges();
+
+            // Act
+            var users = repo.GetUsers();
+
+            // Assert
+            Assert.AreEqual(2, users.Count());
+        }
+
+        [TestMethod]
+        public void Users_GetUsers_EmptyList() {
+            // Arrange
+            var repo = new UsersRepository(context);
+            var usersToDelete = (from u in context.Users
+                                select u).ToList();
+            foreach(User user in usersToDelete) {
+                user.Deleted = true;
+            }
+            context.SaveChanges();
+
+            // Act
+            var users = repo.GetUsers();
+
+            // Assert
+            Assert.AreEqual(0, users.Count());
+        }
+
+        [TestMethod]
+        public void Users_GetUserById_ThatExists() {
+            // Arrange
+            var repo = new UsersRepository(context);
+            int id = (context.Users.Where(u => u.Name == NAME_DABS).SingleOrDefault()).Id;
+
+            // Act
+            var user = repo.GetUserById(id);
+
+            // Assert
+            Assert.AreEqual(NAME_DABS, user.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public void Users_GetUserById_ThatDoesntExist() {
+            // Arrange
+            var repo = new UsersRepository(context);
+            int id = (context.Books.OrderByDescending(u => u.Id).FirstOrDefault()).Id;
+
+            // Act
+            var user = repo.GetUserById(id+1);
+
+            // Assert
+            Assert.Fail("Should have thrown NotFoundException");
+        }
+
+        [TestMethod]
+        public void Users_DeleteUser_ThatExist() {
+            // Arrange
+            var repo = new UsersRepository(context);
+            int id = (context.Users.Where(u => u.Name == NAME_DABS).SingleOrDefault()).Id;
+
+            // Act
+            repo.DeleteUser(id);
+
+            // Assert
+            Assert.AreEqual(true, (context.Users.Where(u => u.Name == NAME_DABS).SingleOrDefault()).Deleted);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public void Users_DeleteUser_ThatDoesntExist() {
+            // Arrange
+            var repo = new UsersRepository(context);
+            int id = (context.Books.OrderByDescending(u => u.Id).FirstOrDefault()).Id;
+
+            // Act
+            repo.DeleteUser(id+1);
+
+            // Assert
+            Assert.Fail("Should have thrown NotFoundException");
+        }
+
+        [TestMethod]
+        public void Users_UpdateUser_ThatExists() {
+            // Arrange
+            var repo = new UsersRepository(context);
+            int id = (context.Users.Where(u => u.Name == NAME_DABS).SingleOrDefault()).Id;
+            var updatedUser = new UserView {
+                Name = "dabs",
+                Address = ADDRESS_DABS,
+                Email = EMAIL_DABS,
+                PhoneNumber = "55-12345"
             };
 
-            _db.Users.Add(userEntity);
+            // Act
+            repo.UpdateUser(updatedUser, id);
 
-            try{
-                _db.SaveChanges();
-            }
-            catch(System.Exception e){
-                Console.WriteLine(e);
-            }
+            // Assert
+            Assert.AreEqual("dabs", (context.Users.Where(u => u.Id == id).SingleOrDefault()).Name);
+            Assert.AreEqual("55-12345", (context.Users.Where(u => u.Id == id).SingleOrDefault()).PhoneNumber);
+        }
 
-            return new UserDTO {
-                Id = userEntity.Id,
-                Name = userEntity.Name,
-                Address = userEntity.Address,
-                Email = userEntity.Email,
-                PhoneNumber = userEntity.PhoneNumber,
-                LoanHistory = new List<LoanDTO>()
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public void Users_UpdateUser_ThatDoesntExists() {
+            // Arrange
+            var repo = new UsersRepository(context);
+            int id = (context.Books.OrderByDescending(u => u.Id).FirstOrDefault()).Id;
+            var updatedUser = new UserView {
+                Name = "dabs",
+                Address = ADDRESS_DABS,
+                Email = EMAIL_DABS,
+                PhoneNumber = "55-12345"
             };
-        }
 
-        public void DeleteUser(int userId)
-        {
-            var user = (from u in _db.Users
-                        where u.Id == userId
-                        select u).SingleOrDefault();
-            
-            if (user == null) {
-                throw new NotFoundException("User with id " + userId + " does not exist");
-            }
+            // Act
+            repo.UpdateUser(updatedUser, id+1);
 
-            user.Deleted = true;
-
-            try {
-                _db.SaveChanges();
-            }
-            catch(DbUpdateException e) {
-                Console.WriteLine(e);
-            }
-        }
-
-        public void UpdateUser(UserView updatedUser, int userId)
-        {
-            var user = (from u in _db.Users
-                        where u.Id == userId
-                        && u.Deleted == false
-                        select u).SingleOrDefault();
-            
-            if (user == null) {
-                throw new NotFoundException("User with id " + userId + " does not exist");
-            }
-
-            user.Name = updatedUser.Name;
-            user.Address = updatedUser.Address;
-            user.Email = updatedUser.Email;
-            user.PhoneNumber = updatedUser.PhoneNumber;
-            
-            try {
-                _db.SaveChanges();
-            }
-            catch(DbUpdateException e) {
-                Console.WriteLine(e);
-            }
-        }
-
-        public UserDTO GetUserById(int userId)
-        {
-            var user = (from u in _db.Users
-                        where u.Id == userId
-                        && u.Deleted == false
-                        select new UserDTO {
-                            Id = u.Id,
-                            Name = u.Name,
-                            Address = u.Address,
-                            Email = u.Email,
-                            PhoneNumber = u.PhoneNumber,
-                            LoanHistory = (from l in _db.Loans
-                                            where l.UserId == userId
-                                            join b in _db.Books on l.BookId equals b.Id
-                                            select new LoanDTO {
-                                                Id = l.Id,
-                                                BookTitle = b.Title,
-                                                LoanDate = l.LoanDate,
-                                                EndDate = l.EndDate
-                                            }).ToList()
-                        }).SingleOrDefault();
-            if (user == null) {
-                throw new NotFoundException("User with id " + userId + " does not exist");
-            }
-
-            return user;
-        }
-
-        public IEnumerable<UserDTOLite> GetUsers() {
-            var users = (from u in _db.Users
-                        where u.Deleted == false
-                        select new UserDTOLite {
-                            Id = u.Id,
-                            Name = u.Name,
-                            Address = u.Address,
-                            Email = u.Email,
-                            PhoneNumber = u.PhoneNumber
-                        }).ToList();
-            return users;
-        }
-
-        public IEnumerable<UserDTOLite> GetUsersOnLoan(DateTime queryDate){
-            var users = (from u in _db.Users
-                        join l in _db.Loans
-                            on u.Id equals l.UserId 
-                            where queryDate >= l.LoanDate  
-                          && (queryDate < l.EndDate || l.EndDate == null)
-                          select new UserDTOLite{
-                              Id = u.Id,
-                              Name = u.Name,
-                              Address = u.Address,
-                              Email = u.Email,
-                              PhoneNumber = u.PhoneNumber
-                          }
-            ).ToList().Distinct();
-            return users;
+            // Assert
+            Assert.Fail("Should have thrown a NotFoundException");
         }
     }
 }
