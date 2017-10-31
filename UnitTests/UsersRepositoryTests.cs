9accd6e6efbfd7f233342265bef14d92599cf5db
Gudjon <gudjonss12@ru.is>
2017-10-31 20:08:05 +0000

    hmm strange

--- UnitTests/UsersRepositoryTests.cs
+++ UnitTests/UsersRepositoryTests.cs
@@ -1,52 +1,159 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
+using LibraryAPI.Models.DTOModels;
 using LibraryAPI.Models.ViewModels;
-using LibraryAPI.Repositories;
+using LibraryAPI.Models.EntityModels;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryAPI.Exceptions;
 
-namespace LibraryAPI.UnitTests
+namespace LibraryAPI.Repositories
 {
-    [TestClass]
-    public class UsersRepositoryTests
+    public class UsersRepository : IUsersRepository
     {
-        private const string NAME_DABS  = "Daníel B. Sigurgeirsson";
-        private const string ADDRESS_DABS  = "Rauðrófustígur 14";
-        private const string EMAIL_DABS  = "dabs@ru.is";
-        private const string PHONE_DABS = "1234567";
+        private readonly AppDataContext _db;
 
-        [TestMethod]
-        public void AddUserToDatabase()
-        {
-            // Arrange
-            var options = new DbContextOptionsBuilder<AppDataContext>()
-                .UseInMemoryDatabase(databaseName: "Add_writes_to_database")
-                .Options;
+        public UsersRepository(AppDataContext db) {
+            _db = db;
+        }
 
-            // Act
-            // Run the test against one instance of the context
-            using (var context = new AppDataContext(options))
-            {
-                var service = new UsersRepository(context);
-                service.AddUser(new UserView {
-                    Name = NAME_DABS,
-                    Address = ADDRESS_DABS,
-                    Email = EMAIL_DABS,
-                    PhoneNumber = PHONE_DABS
-                });
+        public UserDTO AddUser(UserView user) {
+            var userCheck = (from u in _db.Users
+                            where user.Email == u.Email
+                            && u.Deleted == false
+                            select u).SingleOrDefault();
+            
+            if (userCheck != null) {
+                throw new AlreadyExistsException("The request could not be completed due to a conflict with the current state of the resource.");
             }
 
-            // Assert
-            // Use a separate instance of the context to verify correct data was saved to database
-            using (var context = new AppDataContext(options))
-            {
-                Assert.AreEqual(1, context.Users.Count());
-                Assert.AreEqual(NAME_DABS, context.Users.Single().Name);
+            var userEntity = new User {
+                Name = user.Name,
+                Address = user.Address,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                Deleted = false
+            };
+
+            _db.Users.Add(userEntity);
+
+            try{
+                _db.SaveChanges();
+            }
+            catch(System.Exception e){
+                Console.WriteLine(e);
+            }
+
+            return new UserDTO {
+                Id = userEntity.Id,
+                Name = userEntity.Name,
+                Address = userEntity.Address,
+                Email = userEntity.Email,
+                PhoneNumber = userEntity.PhoneNumber,
+                LoanHistory = new List<LoanDTO>()
+            };
+        }
+
+        public void DeleteUser(int userId)
+        {
+            var user = (from u in _db.Users
+                        where u.Id == userId
+                        select u).SingleOrDefault();
+            
+            if (user == null) {
+                throw new NotFoundException("User with id " + userId + " does not exist");
+            }
+
+            user.Deleted = true;
+
+            try {
+                _db.SaveChanges();
+            }
+            catch(DbUpdateException e) {
+                Console.WriteLine(e);
             }
         }
-        [TestMethod]
-        public void GetBookByIdFromDatabase(){
+
+        public void UpdateUser(UserView updatedUser, int userId)
+        {
+            var user = (from u in _db.Users
+                        where u.Id == userId
+                        && u.Deleted == false
+                        select u).SingleOrDefault();
             
+            if (user == null) {
+                throw new NotFoundException("User with id " + userId + " does not exist");
+            }
+
+            user.Name = updatedUser.Name;
+            user.Address = updatedUser.Address;
+            user.Email = updatedUser.Email;
+            user.PhoneNumber = updatedUser.PhoneNumber;
+            
+            try {
+                _db.SaveChanges();
+            }
+            catch(DbUpdateException e) {
+                Console.WriteLine(e);
+            }
+        }
+
+        public UserDTO GetUserById(int userId)
+        {
+            var user = (from u in _db.Users
+                        where u.Id == userId
+                        && u.Deleted == false
+                        select new UserDTO {
+                            Id = u.Id,
+                            Name = u.Name,
+                            Address = u.Address,
+                            Email = u.Email,
+                            PhoneNumber = u.PhoneNumber,
+                            LoanHistory = (from l in _db.Loans
+                                            where l.UserId == userId
+                                            join b in _db.Books on l.BookId equals b.Id
+                                            select new LoanDTO {
+                                                Id = l.Id,
+                                                BookTitle = b.Title,
+                                                LoanDate = l.LoanDate,
+                                                EndDate = l.EndDate
+                                            }).ToList()
+                        }).SingleOrDefault();
+            if (user == null) {
+                throw new NotFoundException("User with id " + userId + " does not exist");
+            }
+
+            return user;
+        }
+
+        public IEnumerable<UserDTOLite> GetUsers() {
+            var users = (from u in _db.Users
+                        where u.Deleted == false
+                        select new UserDTOLite {
+                            Id = u.Id,
+                            Name = u.Name,
+                            Address = u.Address,
+                            Email = u.Email,
+                            PhoneNumber = u.PhoneNumber
+                        }).ToList();
+            return users;
+        }
+
+        public IEnumerable<UserDTOLite> GetUsersOnLoan(DateTime queryDate){
+            var users = (from u in _db.Users
+                        join l in _db.Loans
+                            on u.Id equals l.UserId 
+                            where queryDate >= l.LoanDate  
+                          && (queryDate < l.EndDate || l.EndDate == null)
+                          select new UserDTOLite{
+                              Id = u.Id,
+                              Name = u.Name,
+                              Address = u.Address,
+                              Email = u.Email,
+                              PhoneNumber = u.PhoneNumber
+                          }
+            ).ToList().Distinct();
+            return users;
         }
     }
 }
